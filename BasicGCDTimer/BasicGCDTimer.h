58115aab6c2be66f7e4dd8ff82b2244d38c0801d
Chris M <webmaster@magnaws.com>
2019-01-07 07:59:48 -0500

    Initial Commit

---
+++ BasicGCDTimer/BasicGCDTimer.h
@@ -0,0 +1,19 @@
+//
+//  BasicGCDTimer.h
+//  BasicGCDTimer
+//
+//  Created by Chris Marshall on 1/7/19.
+//  Copyright © 2019 The Great Rift Valley Software Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicGCDTimer.
+FOUNDATION_EXPORT double BasicGCDTimerVersionNumber;
+
+//! Project version string for BasicGCDTimer.
+FOUNDATION_EXPORT const unsigned char BasicGCDTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicGCDTimer/PublicHeader.h>
+
+
